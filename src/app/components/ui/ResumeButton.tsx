d70efc17d7c4d9331f234db6ab7fe0dd2a5418a0
Onur Avan <118551340+onurvn@users.noreply.github.com>
2024-10-23 13:58:53 +0300

    Update ResumeButton.tsx

--- src/app/components/ui/ResumeButton.tsx
+++ src/app/components/ui/ResumeButton.tsx
@@ -6,7 +6,7 @@ import { FileDown } from "lucide-react";
 const ResumeButton: React.FC = () => {
     return (
         <a
-            href="https://drive.google.com/file/d/1ErKmCf1fRbrbUAicAgyqFVqY_zaKZ86x/view?usp=sharing"
+            href="https://drive.google.com/file/d/14nAMUgtwQ9MGiJ7d1M-EWJIbnuPkwF0h/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center hover:bg-white text-white hover:text-[#111111] px-3 py-1 rounded transition duration-300 mr-4 border border-white"
